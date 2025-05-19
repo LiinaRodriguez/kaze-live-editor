ac312a84fc1f88c468e52868fbdd269c714700d4
LinaRodriguez <lsrodriguez@unillanos.edu.co>
2025-05-19 08:33:42 -0500

    node visualization

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,12 @@
-import { useState } from 'react';
-import { parseToReactFlow } from 'kaze';
-import type { KazeEdge, KazeNode } from 'kaze';
-
-import './App.css';
-
-function App() {
-  const [nodes, setNodes] = useState<KazeNode[]>([]);
-  const [edges, setEdges] = useState<KazeEdge[]>([]);
-
-  const kazefile = `
-    root{
-      label: "Raiz",
-      children: [node1, node2, node3]
-      style: {color: #ffb3ba,
-      bgcolor: #fffff}
-    };
-
-    node1{
-      label: "Node 1",
-      style:{
-        color: #bae1ff
-        }
-    }
-
-    node2{
-      label: "Node 3",
-      style:{ color: #bae1ff}
-    }
-
-    node3{
-      label: "Node 14",
-      style: {color: #bae1ff}
-    }
-  `;
-
-  const handleParse = () => {
-    const { nodes, edges } = parseToReactFlow(kazefile);
-    setNodes(nodes);
-    setEdges(edges);
-  };
-
+import Visualization from './components/visualization'
+ 
+export default function App() {
   return (
-    <div className="App">
-      <button onClick={handleParse}>Parse Kaze Code</button>
-
-      <div>
-        <h3>Nodes:</h3>
-        <pre>{JSON.stringify(nodes, null, 2)}</pre>
-      </div>
-      <div>
-        <h3>Edges:</h3>
-        <pre>{JSON.stringify(edges, null, 2)}</pre>
+    <div style={{ display: 'flex', width: '100vw', height: '100vh' }}>
+      <div style={{ width: '50%', borderRight: '1px solid #ccc' }}>Editor</div>
+      <div style={{ width: '50%' }}>
+        <Visualization />
       </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/data/test.kaze.ts
@@ -0,0 +1,23 @@
+export const kazeFile = `root{
+      label: "Raiz",
+      children: [node1, node2, node3]
+      style: {color: #ffb3ba,
+      bgcolor: #fffff}
+    };
+
+    node1{
+      label: "Node 1",
+      style:{
+        color: #bae1ff
+        }
+    }
+
+    node2{
+      label: "Node 3",
+      style:{ color: #bae1ff}
+    }
+
+    node3{
+      label: "Node 14",
+      style: {color: #bae1ff}
+    }`
